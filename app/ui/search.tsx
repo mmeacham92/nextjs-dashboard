b9524c6f1ba5eb6527afed9f6f8d79586f038850
mmeacham92 <meacham.michael@rocketmail.com>
2024-01-28 12:10:22 -0600

    added defaultValue attribute to input element within /ui/search.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname } from 'next/navigation';
-import { useRouter } from 'next/router';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -34,6 +33,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => handleSearch(e.target.value)}
+        
+        // to ensure the input field is in sync with the URL and will be populated when sharing, we add a defaultValue attribute to the input element by reading from searchParams
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>

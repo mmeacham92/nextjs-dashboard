6f1bc163bdc7ec55398c5d263f2a86c379830167
mmeacham92 <meacham.michael@rocketmail.com>
2024-01-28 12:33:16 -0600

    implemented 'debouncing' in order to prevent a database query on every keystroke; installed and imported useDebouncedCallback into /ui/search.tsx so that a database query will only happen 300ms after the last keystroke'

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,37 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  // -------------------------------------
+  // function handleSearch(term: string) {
+  //   console.log(`Searching... ${term}`);
+
+  //   const params = new URLSearchParams(searchParams);
+  //   if (term) {
+  //     params.set('query', term);
+  //   } else {
+  //     params.delete('query');
+  //   }
+
+  //   replace(`${pathname}?${params.toString()}`)
+  // }
+
+  // the above version of handleSearch would cause a database query on every keystroke
+  // this causes an issue when applications scale
+  // to solve this issue, we can use 'debouncing', which will limit the number of queries made
+  // based on a defined wait time.
+  // to do so, we import useDebouncedCallback, then wrap the body of the above function as a callback in it as an argument. we also provide a ms value as a second argument
+  // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
     // if the user has entered text, set it as the value of the query, else delete it
     if (term) {
@@ -16,13 +40,13 @@ export default function Search({ placeholder }: { placeholder: string }) {
     } else {
       params.delete('query');
     }
-    
+
     replace(`${pathname}?${params.toString()}`)
     // ${pathname} is the current path
     // As the user types intot he search bar, params.toString() translates this input into a URL-friendly format
     // the replace call updates the URL with the user's search data
     // the URL is updated without reloading to the page (Next.js client side navigation)
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -33,7 +57,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => handleSearch(e.target.value)}
-        
+
         // to ensure the input field is in sync with the URL and will be populated when sharing, we add a defaultValue attribute to the input element by reading from searchParams
         defaultValue={searchParams.get('query')?.toString()}
       />

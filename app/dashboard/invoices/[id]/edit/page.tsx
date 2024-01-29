e20ff73acdd05ada7817220e4aed62aa9089b209
mmeacham92 <meacham.michael@rocketmail.com>
2024-01-29 13:48:50 -0600

    created updateInvoice function within action.ts; added updateInvoice function as Server action on edit-form.tsx form element

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,32 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const id = params.id;
+    
+    // could do this, but will happen one at a time:
+    // const customers = await fetchCustomers();
+    // const invoice = await fetchInvoiceById(id);
+    // instead, we can use Promise.all to execute both async functions in parallel:
+    const [invoice, customers] = await Promise.all([
+        fetchInvoiceById(id),
+        fetchCustomers()
+    ]);
+    
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Invoices', href: '/dashboard/invoices' },
+                    {
+                        label: 'Edit Invoice',
+                        href: `/dashboard/invoices/${id}/edit`,
+                        active: true,
+                    },
+                ]}
+            />
+            <Form invoice={invoice} customers={customers} />
+        </main>
+    );
+}
--- app/lib/action.ts
+++ app/lib/action.ts
@@ -5,7 +5,8 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-
+// CREATE
+// -----------------------------
 // create an object using Zod library (which will validate types for us)
 const FormSchema = z.object({
     id: z.string(),
@@ -38,5 +39,29 @@ export async function createInvoice(formData: FormData) {
     // cause browser to fetch from database to update cache
     revalidatePath('/dashboard/invoices');
     // redirect back to the invoices page
+    redirect('/dashboard/invoices');
+}
+
+
+// UPDATE
+// ---------------------
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status')
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+    `
+    revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }

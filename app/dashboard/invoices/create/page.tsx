7d10cb463bffdfd2b6f5fc34afcc151789275bbf
mmeacham92 <meacham.michael@rocketmail.com>
2024-01-29 12:46:10 -0600

    created action.ts file within /lib folder to store all Server Actions; created createInvoice function which accepts a FormData object, deconstructs and validates it using Zod library, and inserts data into database

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -5,6 +5,8 @@ import { fetchCustomers } from '@/app/lib/data';
 export default async function Page() {
     const customers = await fetchCustomers();
 
+
+
     return (
         <main>
             <Breadcrumbs

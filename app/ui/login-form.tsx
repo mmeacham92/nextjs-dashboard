1527ba657d66530a8d42b199a7f0acb5f2146b81
mmeacham92 <meacham.michael@rocketmail.com>
2024-01-30 12:20:26 -0600

    forgot to make login-form.tsx a client-side component

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
@@ -13,7 +15,7 @@ export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
   return (
-    <form actin={dispatch} className="space-y-3">
+    <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
